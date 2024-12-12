bfad7c464c91e38f9acf7c1e4ecab6b8430def5b
JaguarsCodehub <techbrains21@gmail.com>
2024-12-12 17:58:55 +0530

    Feature: changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   images: {
     domains: ['images.unsplash.com', 'upload.wikimedia.org'],
   },
+  assetPrefix: '/',
+  basePath: '',
+  trailingSlash: true,
 };
 
 export default nextConfig;
