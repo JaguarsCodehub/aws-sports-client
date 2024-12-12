693c20e75c4d67ff0c134944b0511484b0902aa4
Jyotindra Tavanoji <tavanojijyotindra@gmail.com>
2024-12-12 21:01:45 +0530

    env changesasda

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
   images: {
     domains: ['images.unsplash.com', 'upload.wikimedia.org'],
   },
-  assetPrefix: '/',
-  basePath: '',
-  trailingSlash: true,
 };
 
 export default nextConfig;
